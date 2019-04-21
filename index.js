db960d5a29de8c799d1e6c536534aea1f6fcdecb
Mark DeLuzio <deluziomark@gmail.com>
2019-04-20 20:14:46 -0400

    made addListItems function

--- index.js
+++ index.js
@@ -3,5 +3,27 @@ function handleChecked() {
         let checkedItem = $(this).closest('li').find('span.shopping-item');
         $(checkedItem).toggleClass('shopping-item__checked');
     })
-}
+};
+
 $(handleChecked);
+
+
+function addListItem() {
+    $('#js-shopping-list-form').submit(function(event){
+        event.preventDefault();
+        let newItem = $('#shopping-list-entry').val();
+        let newLi = `<span class="shopping-item">` + newItem + `</span>
+    <div class="shopping-item-controls">
+      <button class="shopping-item-toggle">
+        <span class="button-label">check</span>
+      </button>
+      <button class="shopping-item-delete">
+        <span class="button-label">delete</span>
+      </button>
+    </div>`;
+        $('.shopping-list').append('<li>' + newLi + '</li>');
+        $('input').val('');
+    });
+};
+
+$(addListItem);
